7ede032d1312318a299a16d832b93658c7205cee
marcos pirela <marcosj465@gmail.com>
2020-05-21 10:36:13 -0400

    Agregada logica saber costos de cocacola

--- logicaCocacolin.js
+++ logicaCocacolin.js
@@ -8,7 +8,15 @@ fetch('https://s3.amazonaws.com/dolartoday/data.json')
 
 
 .then(function(neko) {
-    console.log(neko.USD.transferencia);
+
+    var dolarValor = neko.USD.transferencia;
+    var retornablevalor = 0.74;
+    var pesisvalor = 1.07;
+
+    var totalRetornable = retornablevalor * neko.USD.transferencia ;
+    var totalpesis =  pesisvalor* neko.USD.transferencia ;
+    console.log(totalRetornable);
+    console.log(totalpesis);
     precio.innerHTML='<em>El precio de la coca cola es:</em> <strong>'+neko.USD.transferencia+'</strong>';
   
 }); 
