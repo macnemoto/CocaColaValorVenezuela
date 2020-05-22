9c9e6e4ea3f24b5742efe17b7252b86da348ebe0
marcos pirela <marcosj465@gmail.com>
2020-05-22 02:25:53 -0400

    Cambios a logica v2

--- logicaCocacolin.js
+++ logicaCocacolin.js
@@ -22,11 +22,17 @@ fetch('https://s3.amazonaws.com/dolartoday/data.json')
     var enteroPesis = totalpesis.toFixed(); 
     var redondeoRetornable = 0 ;
     redondeoRetornable = Math.round(0.74);
+    
+    dolarValor = (new Intl.NumberFormat("de-DE", {maximumSignificantDigits: 6}).format(dolarValor));
+    enteroPesis = (new Intl.NumberFormat("de-DE", {maximumSignificantDigits: 3}).format(enteroPesis));
+    enteroRetirnable = (new Intl.NumberFormat("de-DE", {maximumSignificantDigits: 3}).format(enteroRetirnable));
+
+
 
     console.log(redondeoRetornable);
     console.log(enteroRetirnable);
     console.log(enteroPesis);
-    precio.innerHTML='<em>El precio de la lechuga es:</em> <strong>'+neko.USD.transferencia+' $</strong>';
+    precio.innerHTML='<em>El precio de la lechuga es:</em> <strong>'+dolarValor+' $</strong>';
   pesiss.innerHTML='<em>La Pesis 1/2 esta en:</em> <strong>'+enteroPesis+' BsS</strong>';
  retornablee.innerHTML='<em>La Retornable esta en:</em> <strong>'+enteroRetirnable+' BsS</strong>';
 }); 
